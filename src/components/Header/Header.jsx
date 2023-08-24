fd41640e933d7bb76851610d649581bf4191a7cb
Brayan Ceron <brayan.ceron@edunext.co>
2023-08-24 09:44:10 -0500

    feat: Create Validation Panel layout; create header component
    * Create a mock from the required data while is integrated the api

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,25 @@
-const Header = () => (
-  <div>Header</div>
+import { intlShape } from '@edx/frontend-platform/i18n';
+import {
+  Container, Icon, IconButton, Stack,
+} from '@edx/paragon';
+import { ArrowBack } from '@edx/paragon/icons';
+import messages from '../../i18n';
+
+const Header = ({ intl }) => (
+  <Stack className="bg-secondary-200">
+    <Container>
+      <IconButton
+        className="my-3 mx-5"
+        src={ArrowBack}
+        iconAs={Icon}
+        alt={intl.formatMessage(messages.goBack)}
+      />
+    </Container>
+  </Stack>
 );
+
+Header.propTypes = {
+  intl: intlShape.isRequired,
+};
 
 export default Header;
---
+++ src/data/mocked_data.js
@@ -0,0 +1,48 @@
+export default [
+  {
+    course_name: 'Test Course #1',
+    course_id: 'course-v1:org1+test-course-1',
+    organization: 'Organization 1',
+    categories: ['Category 1', 'Category 2'],
+    validation_body: 'Validator Body #1',
+    validation_process_events: [
+      {
+        status: 'Submitted',
+        created_at: '2023-08-01',
+        reason: null,
+        comment: 'This is the comment of the course author',
+        user: 'Course Author 1',
+      },
+      {
+        status: 'Disapproved',
+        created_at: '2023-08-15',
+        reason: 'Reason 1',
+        comment: 'This is the comment of Validator 1',
+        user: 'Validator 1',
+      },
+    ],
+  },
+  {
+    course_name: 'Test Course #2',
+    course_id: 'course-v1:org2+test-course-2',
+    organization: 'Organization 2',
+    categories: ['Category 1', 'Category 3'],
+    validation_body: 'Validator Body #2',
+    validation_process_events: [
+      {
+        status: 'Submitted',
+        created_at: '2023-08-10',
+        reason: null,
+        comment: 'This is another comment of the course author',
+        user: 'Course Author 2',
+      },
+      {
+        status: 'Approved',
+        created_at: '2023-08-20',
+        reason: null,
+        comment: 'This is the comment of Validator 2',
+        user: 'Validator 2',
+      },
+    ],
+  },
+];
