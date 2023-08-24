6ebe9eb878d27da939fe915e74dfa48ef44f3d9b
Brayan Ceron <brayan.ceron@edunext.co>
2023-08-24 09:46:25 -0500

    feat: Create table to show course validation process
    * Add helpers to adapt data from api to the table requirements
    * Add some possible translations

---
+++ src/messages.js
@@ -0,0 +1,30 @@
+import { defineMessages } from '@edx/frontend-platform/i18n';
+
+const messages = defineMessages({
+  heading: {
+    id: 'course-authoring.validations-panel.heading',
+    defaultMessage: 'Validation Panel',
+  },
+  settings: {
+    id: 'course-authoring.validations-panel.resources.settings.button',
+    defaultMessage: 'settings',
+  },
+  goBack: {
+    id: 'course-authoring.validations-panel.goBack.button',
+    defaultMessage: 'Go back',
+  },
+  viewLiveButton: {
+    id: 'course-authoring.validations-panel.viewLive.button',
+    defaultMessage: 'View live',
+  },
+  enabled: {
+    id: 'course-authoring.badge.enabled',
+    defaultMessage: 'Enabled',
+  },
+  contentPermissions: {
+    id: 'course-authoring.validations-panel.content-permissions.heading',
+    defaultMessage: 'Content permissions',
+  },
+});
+
+export default messages;
