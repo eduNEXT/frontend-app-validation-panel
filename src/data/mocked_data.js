af40c4e646db75b7405f7ed1cb3ed449a5a4f990
Brayan Ceron <brayan.ceron@edunext.co>
2023-08-24 11:42:30 -0500

    feat: Add and action button in data table
    * Add more data to the table to test Actions availability

--- src/data/mocked_data.js
+++ src/data/mocked_data.js
@@ -15,7 +15,7 @@ export default [
       },
       {
         status: 'Disapproved',
-        created_at: '2023-08-15',
+        created_at: '2023-08-10',
         reason: 'Reason 1',
         comment: 'This is the comment of Validator 1',
         user: 'Validator 1',
@@ -38,6 +38,52 @@ export default [
       },
       {
         status: 'Approved',
+        created_at: '2023-08-20',
+        reason: null,
+        comment: 'This is the comment of Validator 2',
+        user: 'Validator 2',
+      },
+    ],
+  },
+  {
+    course_name: 'Test Course #2',
+    course_id: 'course-v1:org2+test-course-2',
+    organization: 'Organization 2',
+    categories: ['Category 1', 'Category 3'],
+    validation_body: 'Validator Body #2',
+    validation_process_events: [
+      {
+        status: 'Submitted',
+        created_at: '2023-08-10',
+        reason: null,
+        comment: 'This is another comment of the course author',
+        user: 'Course Author 2',
+      },
+      {
+        status: 'Approved',
+        created_at: '2023-08-02',
+        reason: null,
+        comment: 'This is the comment of Validator 2',
+        user: 'Validator 2',
+      },
+    ],
+  },
+  {
+    course_name: 'Test Course #2',
+    course_id: 'course-v1:org2+test-course-2',
+    organization: 'Organization 2',
+    categories: ['Category 1', 'Category 3'],
+    validation_body: 'Validator Body #2',
+    validation_process_events: [
+      {
+        status: 'Submitted',
+        created_at: '2023-08-10',
+        reason: null,
+        comment: 'This is another comment of the course author',
+        user: 'Course Author 2',
+      },
+      {
+        status: 'Draft',
         created_at: '2023-08-20',
         reason: null,
         comment: 'This is the comment of Validator 2',
