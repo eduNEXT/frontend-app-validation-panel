77c0d3dfe1eb53d0aaeec9a6e73c4104a05e7669
Diana Catalina Olarte <diana.olarte@edunext.co>
2023-08-28 16:26:08 +1000

    feat: create the api conexion funtions

---
+++ src/data/constants.js
@@ -0,0 +1,32 @@
+const VALIDATION_STATUS = {
+  SUBMITTED: 'Submitted',
+  IN_REVIEW: 'In Review',
+  DRAFT: 'Draft',
+  APPROVED: 'Approved',
+  DISAPPROVED: 'Disapproved',
+  CANCELLED: 'Cancelled',
+  EXEMPT: 'Exempt',
+};
+
+const VALIDATION_ACCESS_ROLE = {
+  AUTHOR: 'author',
+  VALIDATOR: 'validator',
+};
+
+const VALIDATION_ACTION = {
+  SUBMIT: 'Submit for validation',
+  CANCEL: 'Cancel validation',
+};
+const VALIDATION_API_PATH = {
+  COURSE_CATEGORY: 'course-category',
+  VALIDATION_BODY: 'validation-body',
+  VALIDATION_PROCESS: 'validation-process',
+  VALIDATION_EVENT: 'validation-process-event',
+};
+
+export {
+  VALIDATION_ACCESS_ROLE,
+  VALIDATION_ACTION,
+  VALIDATION_API_PATH,
+  VALIDATION_STATUS,
+};
