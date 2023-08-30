d4e0c5f8ca00f9ee401011ec566f519ce3812c01
Diana Olarte <diana.olarte@edunext.co>
2023-08-31 02:38:06 +1000

    feat: create a Timeline component (#3)
    * feat: add Record component
    
    * feat: create Timeline component

---
+++ src/components/Timeline/Timeline.test.jsx
@@ -0,0 +1,55 @@
+import { render } from '@testing-library/react';
+import { IntlProvider } from '@edx/frontend-platform/i18n';
+import Timeline from './Timeline';
+
+const mockEvents = [
+  {
+    status: 'Submitted',
+    createdAt: '2023-08-10',
+    reason: null,
+    comment: 'This is another comment of the course author',
+    user: 'Course Author 2',
+    validationBody: 'Validator Body #1',
+
+  },
+  {
+    status: 'Approved',
+    createdAt: '2023-08-20',
+    reason: null,
+    comment: 'This is the comment of Validator 2',
+    user: 'Validator 2',
+  },
+];
+
+const mockData = { passProcessEvents: mockEvents, validationBody: 'Validator Body #1' };
+
+const renderComponent = (props) => render(
+  <IntlProvider locale="en">
+    <Timeline {...props} />
+  </IntlProvider>,
+);
+
+describe('Timeline', () => {
+  const mockCreatedAt = new Date(mockEvents[0].createdAt);
+  const formatedMockDate = new Intl.DateTimeFormat('en', { year: 'numeric', month: 'short', day: 'numeric' }).format(mockCreatedAt);
+
+  it('should create a list items to display the past process events information', () => {
+    const { container, getByText, getAllByText } = renderComponent(mockData);
+
+    expect(container.querySelectorAll('li').length).toBe(2);
+    expect(getByText(formatedMockDate, { exact: false })).toBeInTheDocument();
+    expect(getByText(mockEvents[0].comment, { exact: false })).toBeInTheDocument();
+    expect(getByText(mockEvents[0].status, { exact: false })).toBeInTheDocument();
+    expect(getAllByText(mockData.validationBody, { exact: false }).length).toBe(2);
+  });
+
+  it('should not render the top line when is the first element', () => {
+    const { container } = renderComponent(mockData);
+    expect(container.querySelector('ul').firstChild.querySelector('.record-line-top')).not.toBeInTheDocument();
+  });
+
+  it('should not render bottom divider when it is the last element', () => {
+    const { container } = renderComponent(mockData);
+    expect(container.querySelector('ul').lastChild.querySelector('article').className).toBe('mb-3 pb-4');
+  });
+});
