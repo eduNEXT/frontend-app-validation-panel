e179374e3f01590e85432608eaac8d2a4ed809f7
Brayan Ceron <brayan.ceron@edunext.co>
2023-09-18 16:19:07 -0500

    refactor: update processes when is submitted a new

--- src/data/slices/validationRecordSlice.js
+++ src/data/slices/validationRecordSlice.js
@@ -3,6 +3,21 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { getAllValidationProcesses, postValidationProcess } from '../api';
 import { REQUEST_STATUS } from '../constants';
 
+const transformValidationProcess = (validationProcess) => ({
+  courseName: validationProcess.course.name || 'None',
+  courseId: validationProcess.course.id,
+  organization: validationProcess.organization.name,
+  categories: validationProcess.categories.map((category) => category.name),
+  validationBody: validationProcess.validationBody.name,
+  validationProcessEvents: validationProcess.events.map((event) => ({
+    status: event.status,
+    createdAt: event.createAt,
+    reason: event.reason,
+    comment: event.comment,
+    user: event.user,
+  })),
+});
+
 export const getAvailableValidationProcesses = createAsyncThunk('organization/validationProcesses', async () => {
   const response = await getAllValidationProcesses();
   return response;
@@ -23,63 +38,29 @@ export const validationRecordSlice = createSlice({
   initialState: validationProcessesInitialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(getAvailableValidationProcesses.pending, (state) => {
-      state.availableValidationProcesses = {
-        loadStatus: REQUEST_STATUS.LOADING,
-        data: [],
-        error: {},
-      };
-    });
-    builder.addCase(getAvailableValidationProcesses.fulfilled, (state, action) => {
-      state.availableValidationProcesses = {
-        loadStatus: REQUEST_STATUS.LOADED,
-        data: action.payload.map((validationProcess) => ({
-          courseName: validationProcess.course.name || 'None',
-          courseId: validationProcess.course.id,
-          organization: validationProcess.organization.name,
-          categories: validationProcess.categories.map((category) => category.name),
-          validationBody: validationProcess.validationBody.name,
-          validationProcessEvents: validationProcess.events.map((event) => ({
-            status: event.status,
-            createdAt: event.createAt,
-            reason: event.reason,
-            comment: event.comment,
-            user: event.user,
-          })),
-        })),
-        error: {},
-      };
-    });
-    builder.addCase(getAvailableValidationProcesses.rejected, (state, action) => {
-      state.availableValidationProcesses = {
-        loadStatus: REQUEST_STATUS.FAILED,
-        data: [],
-        error: action.error,
-      };
-    });
-    builder.addCase(createValidationProcess.pending, (state) => {
-      state.availableValidationProcesses = {
-        ...state.availableValidationProcesses,
-        loadStatus: REQUEST_STATUS.LOADING,
-        data: [],
-        error: {},
-      };
-    });
-    builder.addCase(createValidationProcess.fulfilled, (state) => {
-      // TODO: When the GET validationProcesses exists, solve this
-      state.availableValidationProcesses = {
-        ...state.availableValidationProcesses,
-        loadStatus: REQUEST_STATUS.LOADED,
-        error: {},
-      };
-    });
-    builder.addCase(createValidationProcess.rejected, (state, action) => {
-      state.availableValidationProcesses = {
-        ...state.availableValidationProcesses,
-        loadStatus: REQUEST_STATUS.FAILED,
-        error: action.error,
-      };
-    });
+    builder
+      .addCase(getAvailableValidationProcesses.pending, (state) => {
+        state.availableValidationProcesses.loadStatus = REQUEST_STATUS.LOADING;
+      })
+      .addCase(getAvailableValidationProcesses.fulfilled, (state, action) => {
+        state.availableValidationProcesses.loadStatus = REQUEST_STATUS.LOADED;
+        state.availableValidationProcesses.data = action.payload.map(transformValidationProcess);
+      })
+      .addCase(getAvailableValidationProcesses.rejected, (state, action) => {
+        state.availableValidationProcesses.loadStatus = REQUEST_STATUS.FAILED;
+        state.availableValidationProcesses.error = action.error;
+      })
+      .addCase(createValidationProcess.pending, (state) => {
+        state.availableValidationProcesses.loadStatus = REQUEST_STATUS.LOADING;
+      })
+      .addCase(createValidationProcess.fulfilled, (state, action) => {
+        state.availableValidationProcesses.loadStatus = REQUEST_STATUS.LOADED;
+        state.availableValidationProcesses.data.push(transformValidationProcess(action.payload));
+      })
+      .addCase(createValidationProcess.rejected, (state, action) => {
+        state.availableValidationProcesses.loadStatus = REQUEST_STATUS.FAILED;
+        state.availableValidationProcesses.error = action.error;
+      });
   },
 });
 
